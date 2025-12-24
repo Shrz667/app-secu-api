fd7d4f89b8d96a4e5c094467dea190f2d0264a4d
shyraz <shyrazguedd.pro@gmail.com>
2025-12-24 15:43:21 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace prototype_app_secu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
