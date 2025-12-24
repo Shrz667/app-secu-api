f2de8fdd5fc62bf9a25ae20127e99f40a225edf5
shyraz <shyrazguedd.pro@gmail.com>
2025-12-24 20:01:09 +0100

    v1

---
+++ Models/BDD.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prototype_app_secu.Models
+{
+    public class BDD
+    {
+        
+    }
+}
